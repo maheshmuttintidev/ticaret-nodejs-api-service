2476b87a83fe4fd3e6899054060e9740c5270d22
maheshmuttintidev <maheshmuttintidev@gmail.com>
2024-06-15 14:51:58 +0530

    take mongo_uri from env

--- db/db.config.js
+++ db/db.config.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URI =
-  "mongodb+srv://mahesh1234:Tanuja@143@cluster0.54pav.mongodb.net/registersDB?retryWrites=true&w=majority";
+const MONGO_URI = process.env.MONGO_URI;
 // const MONGO_URI = 'mongodb://localhost/newusers'
 
 exports.connectDB = async () => {
